208ea45ef724852cdd3ad8c3820b8464b4e726c6
Joaquin Loiza <loiza.joaquin@gmail.com>
2020-05-06 20:47:30 -0300

    Se agregó una tabla que segun una categoria dada, muestra los planes existentes

--- views/insurances.views.php
+++ views/insurances.views.php
@@ -17,9 +17,35 @@ class InsurancesView{
         </nav>
         <ul>';
          foreach($seguros as $seguro){
-             echo'<li>'. $seguro->categoria.'</li>';
+             $id=$seguro->id_categoria;
+             echo'<li><a href="vermas/'.$id.'">'. $seguro->categoria.'</a></li>';
          }
         echo '</u></body>
+         </html>';
+    }
+    public function showPlans($planes){
+        echo '<!DOCTYPE html>
+        <html lang="es">
+       <head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Seguros</title>
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+       </head>
+       <body>
+       <nav class="navbar navbar-dark bg-dark">
+       <a class="navbar-brand" href="#">Seguros</a>
+       </nav>
+       <ul>
+       <table class="table table-sm table-dark">
+       <tr><td>Cobertura</td><td>Discripcion</td></tr>';
+       
+       foreach($planes as $plane){
+       echo' <tr><td>'.$plane->cobertura.'</td><td>'.$plane->descripcion.'</td></tr>';
+           
+       }
+
+       echo '</table></u></body>
          </html>';
     }
 
