c5b851355ee62e0c1c1421140205885fd1e50341
Joaquin Loiza <loiza.joaquin@gmail.com>
2020-05-06 20:42:04 -0300

    Se agregó case ver más, al router

--- router.php
+++ router.php
@@ -13,12 +13,16 @@
     // toma la acción que viene del usuario y parsea los parámetros
     $accion = $_GET['action']; 
     $parametros = explode('/', $accion);
-    //var_dump($parametros); die; // like console.log();
+    //var_dump($parametros[0]); die; // like console.log();
 
     // decide que camino tomar según TABLA DE RUTEO
     switch ($parametros[0]) {
         case 'listar': 
             $controller= new InsurancesController();
             $controller->showInsurances();
+        break;
+        case 'vermas':
+            $controller= new InsurancesController();
+            $controller->showPlans($parametros[1]);
         break;
     }
