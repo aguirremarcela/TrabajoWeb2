1d666a76d918e3d016cd59e4ba701206233ace94
Marcela Aguirre <aguirremarcela@hotmail.com.ar>
2020-05-07 19:25:15 -0300

    Realizamos la vista a través de las librerias de Smarty

--- views/insurances.views.php
+++ views/insurances.views.php
@@ -1,24 +1,15 @@
 <?php
-
+require_once ('libs/Smarty.class.php');
 class InsurancesView{
  
     public function showInsurances($seguros){
-        echo '<!DOCTYPE html>
-         <html lang="es">
-        <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Seguros</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-        </head>
-        <body>
-        <nav class="navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Seguros</a>
-        </nav>
-        <ul>';
+        $smarty= new Smarty();
+        $smarty->assign('title','Seguros Marcin');
+        $smarty->display('header.tpl');
+        echo'<ul>';
          foreach($seguros as $seguro){
              $id=$seguro->id_categoria;
-             echo'<li><a href="vermas/'.$id.'">'. $seguro->categoria.'</a></li>';
+             echo'<li><a href="seePlansCategory/'.$id.'">'. $seguro->categoria.'</a></li>';
          }
         echo '</u></body>
          </html>';
@@ -36,16 +27,20 @@ class InsurancesView{
        <nav class="navbar navbar-dark bg-dark">
        <a class="navbar-brand" href="#">Seguros</a>
        </nav>
-       <ul>
-       <table class="table table-sm table-dark">
-       <tr><td>Cobertura</td><td>Discripcion</td></tr>';
+       <ul>';
+    
        
        foreach($planes as $plane){
-       echo' <tr><td>'.$plane->cobertura.'</td><td>'.$plane->descripcion.'</td></tr>';
+
+           $id=$plane->id_planes;
+
+          echo'<li><a href="descripcion/'.$id.'">'. $plane->plan.'</a></li>';
+
+       //echo' <tr><td>'.$plane->cobertura.'</td><td>'.$plane->descripcion.'</td></tr>';
            
        }
 
-       echo '</table></u></body>
+       echo '</ul></body>
          </html>';
     }
 
