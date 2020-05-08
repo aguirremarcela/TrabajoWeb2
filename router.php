fb1b07a81f6985eb57927abc3204b853bf82ee58
Joaquin Loiza <loiza.joaquin@gmail.com>
2020-05-08 12:20:03 -0300

    se agrego un nuevo case showCoverage

--- router.php
+++ router.php
@@ -1,21 +1,15 @@
 <?php
     require_once 'controllers/insurances.controller.php';
 
-
-    // definimos la base url de forma dinamica
     define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
-    // define una acción por defecto
     if (empty($_GET['action'])) {
         $_GET['action'] = 'home';
     } 
 
-    // toma la acción que viene del usuario y parsea los parámetros
     $accion = $_GET['action']; 
     $parametros = explode('/', $accion);
-    //var_dump($parametros[0]); die; // like console.log();
 
-    // decide que camino tomar según TABLA DE RUTEO
     switch ($parametros[0]) {
         case 'home': 
             $controller= new InsurancesController();
@@ -24,5 +18,9 @@
         case 'seePlansCategory':
             $controller= new InsurancesController();
             $controller->showPlans($parametros[1]);
+        break;
+        case 'showCoverage':
+            $controller= new InsurancesController();
+            $controller->showCoverange($parametros[1]);
         break;
     }
