ae6954c243bc75277ede215beba25731a057b0a1
Joaquin Loiza <loiza.joaquin@gmail.com>
2020-05-08 12:18:15 -0300

    refactorizacion y identado del codigo

--- models/insurances.model.php
+++ models/insurances.model.php
@@ -1,35 +1,32 @@
 <?php
+    class InsurancesModel{
+        private function createConection(){
+            $host = 'localhost';
+            $userName = 'root';
+            $password = '';
+            $database = 'db_seguros';
 
- 
- class InsurancesModel{
-    private function createConection(){
-        $host = 'localhost';
-        $userName = 'root';
-        $password = '';
-        $database = 'db_seguros';
-
-        try {
-        $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $userName, $password);
-        } catch (Exception $e) {
-            var_dump($e);
+            try {
+            $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $userName, $password);
+            } catch (Exception $e) {
+                var_dump($e);
+            }
+            return $pdo;
         }
-        return $pdo;
-    }
-
-    public function getCategory(){
-        $db =$this->createConection();
-        $sentencia = $db->prepare("SELECT * FROM categorias"); 
-        $sentencia->execute(); 
-        $categories = $sentencia->fetchAll(PDO::FETCH_OBJ);
-        return($categories);
-    }
-    public function getPlans($id){
-        $db=$this->createConection();
-        $sentencia = $db->prepare("SELECT categorias.categoria, planes.id_planes, planes.plan,
-         planes.cobertura, planes.descripcion FROM categorias JOIN planes ON 
-        categorias.id_categoria=planes.id_categoria_fk WHERE categorias.id_categoria=?");
-        $sentencia->execute([$id]); 
-        $planes = $sentencia->fetchAll(PDO::FETCH_OBJ);
-        return ($planes);
-    }
- }
+        public function getCategory(){
+            $db =$this->createConection();
+            $sentencia = $db->prepare("SELECT * FROM categorias"); 
+            $sentencia->execute(); 
+            $categories = $sentencia->fetchAll(PDO::FETCH_OBJ);
+            return($categories);
+        }
+        public function getPlans($id){
+            $db=$this->createConection();
+            $sentencia = $db->prepare("SELECT categorias.categoria, planes.id_planes, planes.plan,
+             planes.cobertura, planes.descripcion FROM categorias JOIN planes ON 
+            categorias.id_categoria=planes.id_categoria_fk WHERE categorias.id_categoria=?");
+            $sentencia->execute([$id]); 
+            $planes = $sentencia->fetchAll(PDO::FETCH_OBJ);
+            return ($planes);
+        }
+ }  
