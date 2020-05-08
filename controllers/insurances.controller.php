26369431aa93b9d22148c0035a1adc68d49d7d42
Joaquin Loiza <loiza.joaquin@gmail.com>
2020-05-08 12:21:09 -0300

    creacion de la funcion showCoverage

--- controllers/insurances.controller.php
+++ controllers/insurances.controller.php
@@ -1,26 +1,24 @@
 <?php
-require_once 'models/insurances.model.php';
-require_once 'views/insurances.views.php';
+    require_once 'models/insurances.model.php';
+    require_once 'views/insurances.views.php';
 
-class InsurancesController{
-
-    private $model;
-    private $view;
-    public function __construct(){
-        $this->model=new InsurancesModel();
-        $this->view=new InsurancesView();
-
-    }
-    public function showInsurances(){
-        $categorias=$this->model->getCategory();
-     
-        $this->view->showInsurances($categorias);
-    }
-    public function showPlans($id){
-        $planes=$this->model->getPlans($id);
-
-        $this->view->showPlans($planes);
-        
-
-    }
+    class InsurancesController{
+        private $model;
+        private $view;
+        public function __construct(){
+            $this->model=new InsurancesModel();
+            $this->view=new InsurancesView();
+        }
+        public function showInsurances(){
+            $categorias=$this->model->getCategory();
+            $this->view->showInsurances($categorias);
+        }
+        public function showPlans($id){
+            $planes=$this->model->getPlans($id);
+            $this->view->showPlans($planes);
+        }
+        public function showCoverange($id){
+            $coverange=$this->model->getPlans($id);
+            $this->view->showCoverange($coverange);
+        }
 }
