a9dd9a81b21bafb41e92f95abde461453896147c
Joaquin Loiza <loiza.joaquin@gmail.com>
2020-05-14 10:01:05 -0300

    se modifico el codigo para que funcionara con la nueva vista admin.views.php y se agregaron funciones

--- controllers/admin.controller.php
+++ controllers/admin.controller.php
@@ -1,21 +1,22 @@
 <?php
  require_once 'models/insurances.model.php';
- require_once 'views/insurances.views.php';
+ require_once 'views/admin.views.php';
  
  class AdminController{
     private $model;
     private $view;
     public function __construct(){
         $this->model=new InsurancesModel();
-        $this->view=new InsurancesView();
+        $this->view=new adminView();
     }
-    public function showForm(){
-        $categorias=$this->model->getCategory();
-        $this->view->formAdd($categorias);
+    public function showABM(){
+        $this->view->showABM();
+    }
+    public function showAddCategory(){
+        $this->view->formAddCategory();
     }
     public function addCategory(){
         $categoria=$_POST['categoria'];
-        
         if(!empty($categoria)){ 
             $this->model->insertCategory($categoria);
         }
@@ -23,9 +24,11 @@
             echo'<p>no se puede ingresar campo vacio</p>';
         }
     }
-    public function addPlan(){
+    public function showAddplan(){
         $categorias=$this->model->getCategory();
-        $this->view->formAdd($categorias);
+        $this->view->showAddplan($categorias);                                    
+    }
+    public function addPlan(){
         $plan = $_POST['plan'];
         $cobertura=$_POST['cobertura'];
         $descripcion=$_POST['descripcion'];
@@ -38,9 +41,10 @@
             echo"<p>no se puede insertar un</p>";
         }
     }
+    /*
     public function deletePlan(){
         $plans=$this->model->getAllPlans();
         $this->view->showAllPlans($plans);
 
-    }
+    }*/
  }
