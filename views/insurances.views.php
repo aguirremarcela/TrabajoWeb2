08cc53aa6e4d1849765d6f061d901122dcd5c39d
Joaquin Loiza <loiza.joaquin@gmail.com>
2020-05-14 01:16:14 -0300

    refactorizacion de la vista mediante el uso de un constructor

--- views/insurances.views.php
+++ views/insurances.views.php
@@ -1,42 +1,30 @@
 <?php
     require_once ('libs/Smarty.class.php');  
     class InsurancesView{
-
+        private $smarty;
+        public function __construct(){
+            $this->smarty = new Smarty();
+            $this->smarty->assign('base_url', BASE_URL);
+            $this->smarty->assign('title','Seguros Marcin');
+        }
         public function showInsurances($insurances){
-            $smarty= new Smarty();
-            $smarty->assign('base_url', BASE_URL);
-            $smarty->assign('title','Seguros Marcin');
-            $smarty->assign('insurances', $insurances);
-            $smarty->display('insurances.tpl');
+            $this->smarty->assign('insurances', $insurances);
+            $this->smarty->display('insurances.tpl');
         }
         public function showPlans($plans){
-            $smarty= new Smarty();
-            $smarty->assign('base_url', BASE_URL);
-            $smarty->assign('title','Seguros Marcin');
-            $smarty->assign('plans', $plans);
-            $smarty->display('plans.tpl');
+            $this->smarty->assign('plans', $plans);
+            $this->smarty->display('plans.tpl');
         }
         public function showCoverange($coveranges){
-            $smarty= new Smarty();
-            $smarty->assign('base_url', BASE_URL);
-            $smarty->assign('title','Seguros Marcin');
-            $smarty->assign('coveranges', $coveranges);
-            $smarty->display('coveranges.tpl');
+            $this->smarty->assign('coveranges', $coveranges);
+            $this->smarty->display('coveranges.tpl');
         }
         public function formAdd($categorias){
-            $smarty= new Smarty();
-            $smarty->assign('base_url', BASE_URL);
-            $smarty->assign('title','Seguros Marcin');
-            $smarty->assign('categorias',$categorias);
-            $smarty->display('formAdd.tpl');
-
+            $this->smarty->assign('categorias',$categorias);
+            $this->smarty->display('formAdd.tpl');
         }
         public function showAllPlans($plans){
-            $smarty= new Smarty();
-            $smarty->assign('base_url', BASE_URL);
-            $smarty->assign('title','Seguros Marcin');
-            $smarty->assign('plans', $plans);
-            $smarty->display('deletePlans.tpl');
+            $this->smarty->assign('plans', $plans);
+            $this->smarty->display('deletePlans.tpl');
         }
-
 }
