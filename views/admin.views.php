76b04bce8978f9b0be335e32916439c499f51e52
Marcela Aguirre <aguirremarcela@hotmail.com.ar>
2020-05-26 12:33:19 -0300

    Se mudo la funcion que nos muestra el formulario login a otra vista

--- views/admin.views.php
+++ views/admin.views.php
@@ -1,14 +1,11 @@
 <?php
 require_once ('libs/Smarty.class.php');
-class adminView{
+class AdminView{
     private $smarty;
     public function __construct(){
         $this->smarty = new Smarty();
         $this->smarty->assign('base_url', BASE_URL);
         $this->smarty->assign('title','Seguros Marcin');
-    }
-    public function formLogin(){
-        $this->smarty->display('formLogin.tpl');
     }
     public function showABM(){
         $this->smarty->display('optionsAdmin.tpl');
