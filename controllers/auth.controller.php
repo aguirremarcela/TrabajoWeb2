7c7c9ef7f4772538fce6ed7d29e88813808e5ccf
Joaquin Loiza <loiza.joaquin@gmail.com>
2020-05-26 20:29:39 -0300

    se escribio el codigo que inicia una sesion, y se creo la funcion para cerrar dicha sesion

--- controllers/auth.controller.php
+++ controllers/auth.controller.php
@@ -1,12 +1,15 @@
 <?php
 require_once 'models/auth.model.php';
 require_once 'views/auth.view.php';
+require_once 'views/admin.views.php';
 class AuthController{
     private $model;
     private $view;
+    private $adminView;
     public function __construct(){
         $this->model=new AuthModel();
         $this->view=new AuthView();
+        $this->adminView=new AdminView();
     }
     public function showLogin(){
         $this->view->formLogin();
@@ -16,10 +19,20 @@ class AuthController{
         $password=$_POST['password'];
         $user=$this->model->getUser($username);
         if ($user && password_verify($password, $user->password)){
+            session_start();
+            $_SESSION['IS_LOGGED'] = true;
+            $_SESSION['ID_USER'] = $user->id_usuario;
+            $_SESSION['USERNAME'] = $user->email;
+            //$this->adminView->showUser($user);
             header("Location: ".BASE_URL.'showABM');
         }
         else{
             $this->view->formLogin("Los datos son incorrectos");
         }
+    }
+    public function logout() {
+        session_start();
+        session_destroy();
+        header("Location: " . BASE_URL . 'home');
     }
 }
