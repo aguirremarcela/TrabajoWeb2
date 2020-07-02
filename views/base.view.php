d3707d1335240634b0d80c0788fdaffa55f0a1c3
Joaquin Loiza <loiza.joaquin@gmail.com>
2020-07-01 23:54:52 -0300

    se creo base.view.php para ser heredado por las vistas

---
+++ views/base.view.php
@@ -0,0 +1,17 @@
+<?php
+require_once ('libs/smarty/Smarty.class.php');
+class BaseView {
+    private $smarty;
+    public function baseView(){
+        $this->smarty = new Smarty();
+        $this->smarty->assign('base_url', BASE_URL);
+        if(session_status()!= PHP_SESSION_ACTIVE){
+            session_start();
+        }
+        if(isset($_SESSION['IS_LOGGED'])){
+            $this->smarty->assign('user',$_SESSION['USERNAME']);
+            $this->smarty->assign('administrador',$_SESSION['ROLE']);
+        }       
+        return $this->smarty;
+    }
+}
