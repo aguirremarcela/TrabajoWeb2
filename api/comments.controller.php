93853cf476d950c9fcb00b4636ab8550128b905c
Joaquin Loiza <loiza.joaquin@gmail.com>
2020-07-02 19:34:23 -0300

    se creo la funcion getData, addcomment y deleteComment

--- api/comments.controller.php
+++ api/comments.controller.php
@@ -12,15 +12,42 @@ class CommentsController{
         $this->view = new APIView();
         $this->data = file_get_contents("php://input");
     }
-     public function getComments($params = []){
+    public function getData(){
+        return json_decode($this->data);
+    }
+    public function getComments($params = []){
         $idPlan = $params[':ID'];
-        $comments=$this->modelComments->get($idPlan);
+        $comments=$this->modelComments->getAll($idPlan);
         if(empty($comments)){
             $this->view->response("no existen comentarios para el plan con id {$idPlan}", 404);
             die();
         }
         $this->view->response($comments,200);
-     }
-
-
+    }
+    public function addComment($params = []){
+        //Devuelve el objeto JSON enviado por POST.
+        $body = $this->getData();
+        $comment = $body->comentario;
+        $idPlan = $params[':ID'];
+        $score = $body->puntaje;
+        $idUser = $body->id_usuario_fk;
+        //Inserta el comentario.
+        $comment_id = $this->modelComments->insert($comment, $score, $idUser, $idPlan);
+        if($comment_id){
+            $this->view->response("Se agrego el comentario con id: " . $comment_id, 200);
+        }else{
+            $this->view->response("error", 404);
+        }
+    }
+    public function deleteComments($params = []){
+        $idComment = $params[':ID'];
+        $comment=$this->modelComments->get($idComment);
+        if(!empty($comment)){
+            $this->modelComments->delete($idComment);
+            $this->view->response("El comentario ha sido eliminado con exito", 200);
+        }
+        else{
+            $this->view->response("El comentario ha eliminar no se encontro", 404);
+        }
+    }
 }
